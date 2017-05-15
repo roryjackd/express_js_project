5599acea4631cbe7b456c85cb93ab14ea0ab3869
roryjackd <roryjackd@yahoo.com>
2017-05-14 21:46:20 -0600

    1st

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res){
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+    {name: "Granite Hill", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"}
+  ]
+
+  res.render('campgrounds', {campgrounds:campgrounds});
+});
+
+app.post('/campgrounds', function(req, res){
+  res.send('YOU HIT THE POST ROUTE!')
+});
+
+app.get('/campgrounds/new', function(req, res){
+  res.render('new.ejs');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
